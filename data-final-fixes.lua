9fe321a0721c94185043b7c9349040abd55fde1b
Steven Myers <larkenx@gmail.com>
2020-12-24 18:43:15 -0500

    disable upgrade paths for SE pipes because this conflicts with collision masks set by Space Exploration (for underground belts etc there are different ones in space).

---
+++ data-final-fixes.lua
@@ -0,0 +1,7 @@
+-- Pipes
+data.raw.pipe["pipe"].fast_replaceable_group = nil
+data.raw.pipe["pipe"].next_upgrade = nil
+
+-- Underground pipes
+data.raw["pipe-to-ground"]["pipe-to-ground"].fast_replaceable_group = nil
+data.raw["pipe-to-ground"]["pipe-to-ground"].next_upgrade = nil
--- data-updates.lua
+++ data-updates.lua
@@ -1,27 +1,27 @@
--- if krastorio.general.getSafeSettingValue("kr-pipes-and-belts-changes") then
---     -- space underground-belt 
---     if data.raw["underground-belt"]["se-space-underground-belt"] then
---         data.raw["underground-belt"]["se-space-underground-belt"].max_distance = data.raw["underground-belt"]["express-underground-belt"].max_distance
---     end
---     if data.raw.recipe["se-space-underground-belt"] then
---         data.raw.recipe["se-space-underground-belt"].result_count = data.raw.recipe["express-underground-belt"].result_count 
---     end
+if krastorio.general.getSafeSettingValue("kr-pipes-and-belts-changes") then
+    -- space underground-belt 
+    if data.raw["underground-belt"]["se-space-underground-belt"] then
+        data.raw["underground-belt"]["se-space-underground-belt"].max_distance = data.raw["underground-belt"]["express-underground-belt"].max_distance
+    end
+    if data.raw.recipe["se-space-underground-belt"] then
+        data.raw.recipe["se-space-underground-belt"].result_count = data.raw.recipe["express-underground-belt"].result_count 
+    end
 
---     -- space underground-pipe  
---     if data.raw["pipe-to-ground"]["se-space-pipe-to-ground"] then
---         for index, connection in pairs(data.raw["pipe-to-ground"]["se-space-pipe-to-ground"].fluid_box.pipe_connections) do
---             if connection.max_underground_distance then
---             -- see prototypes\vanilla-changes\optional\pipes-and-belts-changes.lua
---                 data.raw["pipe-to-ground"]["se-space-pipe-to-ground"].fluid_box.pipe_connections[index].max_underground_distance = 20
---             end
---         end
---     end
--- end
+    -- space underground-pipe  
+    if data.raw["pipe-to-ground"]["se-space-pipe-to-ground"] then
+        for index, connection in pairs(data.raw["pipe-to-ground"]["se-space-pipe-to-ground"].fluid_box.pipe_connections) do
+            if connection.max_underground_distance then
+            -- see prototypes\vanilla-changes\optional\pipes-and-belts-changes.lua
+                data.raw["pipe-to-ground"]["se-space-pipe-to-ground"].fluid_box.pipe_connections[index].max_underground_distance = 20
+            end
+        end
+    end
+end
 
--- -- SE & K2 Fluids Changes
--- if data.raw.recipe["se-space-pipe-to-ground"] then
---     data.raw.recipe["se-space-pipe-to-ground"].result_count = data.raw.recipe["pipe-to-ground"].result_count 
--- end
+-- SE & K2 Fluids Changes
+if data.raw.recipe["se-space-pipe-to-ground"] then
+    data.raw.recipe["se-space-pipe-to-ground"].result_count = data.raw.recipe["pipe-to-ground"].result_count 
+end
 
 krastorio.technologies.addUnlockRecipe("se-electric-boiler", "se-electric-boiling-steam-415")
 krastorio.technologies.addUnlockRecipe("se-electric-boiler", "se-electric-boiling-steam-975")
@@ -37,7 +37,11 @@ if tank2 then
     tank2.se_allow_in_space = true
 end
 
--- Allow steel pipes
+-- Space Exploration Pipes (Unset the fast replace group)
+-- data.raw["pipe-to-ground"]["se-space-pipe-to-ground"].fast_replaceable_group = nil
+-- data.raw["pipe"]["se-space-pipe"].fast_replaceable_group = nil
+
+-- Allow steel pipes & make them the same collision mask as SE pipes
 steelPipeUnderground = data.raw["pipe-to-ground"]["kr-steel-pipe-to-ground"]
 if steelPipeUnderground then
     steelPipeUnderground.se_allow_in_space = true
@@ -46,6 +50,7 @@ end
 steelPipe = data.raw["pipe"]["kr-steel-pipe"]
 if steelPipe then
     steelPipe.se_allow_in_space = true
+
 end
 
 -- Moon Logic Combinator placeable in Space
