a3c1ba55062fc706612bd2db788a5c9eda11bd5b
Steven Myers <larkenx@gmail.com>
2020-12-25 00:16:30 -0500

    move singularity lab to final data fixes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -5,3 +5,19 @@ data.raw.pipe["pipe"].next_upgrade = nil
 -- Underground pipes
 data.raw["pipe-to-ground"]["pipe-to-ground"].fast_replaceable_group = nil
 data.raw["pipe-to-ground"]["pipe-to-ground"].next_upgrade = nil
+
+
+-- make singularity lab a direct upgrade over space lab. faster speed, but same 
+-- # of modules
+singularityLab = data.raw["lab"]["kr-singularity-lab"]
+spaceScienceLab = data.raw["lab"]["se-space-science-lab"]
+if singularityLab then
+    singularityLab.researching_speed = 15
+    singularityLab.module_specification = {
+        max_entity_info_module_icon_rows = 1,
+        max_entity_info_module_icons_per_row = 7,
+        module_info_icon_shift = { 0, 2 },
+        module_slots = 6
+    }
+    singularityLab.inputs = spaceScienceLab.inputs
+end
