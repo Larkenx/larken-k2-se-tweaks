2b16e2d9c42d0053f7de8dd0497a86e85a872e8b
Nicolas Lang <git@nicolas-lang.de>
2022-01-10 01:49:41 +0100

    moved naquium-processing recipe and tech to K2SE_matterconversion_naquium_data.lua

--- data.lua
+++ data.lua
@@ -1,4 +1,5 @@
 require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_vitamelange_data")
+require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_naquium_data")
 
 local matter_util = require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_util")
 require("__Krastorio2__/lib/public/data-stages/matter-util")
@@ -10,21 +11,6 @@ if mods["Krastorio2"] then
 end
 
 if mods["space-exploration"] and mods["Krastorio2"] then
-    if settings.startup["allow-naquium-matter-processing"].value then
-        matter_util.make_matter_tech("se-kr-matter-naquium-processing", "naquium-processing", 1000)
-        krastorio.matter_func.createMatterRecipe(
-            {
-                item_name = "se-naquium-ore",
-                minimum_conversion_quantity = 10,
-                matter_value = value_multiplier * 20 * 8, -- 40 * 8
-                conversion_matter_value = value_multiplier * 20, -- 400
-                energy_required = value_multiplier * 20,
-                need_stabilizer = true,
-                unlocked_by_technology = "se-kr-matter-naquium-processing"
-            }
-        )
-    end
-
     krastorio.matter_func.createMatterRecipe(
         {
             item_name = "tritium",
