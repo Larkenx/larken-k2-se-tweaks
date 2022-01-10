a6579bbb260acc802804ec268fa0f92b261b41f8
Nicolas Lang <git@nicolas-lang.de>
2022-01-10 03:00:11 +0100

    moved hyper-cooler and particle-accelerator spaceship changes to SE_buildings_on-spaceship_data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,6 +1,7 @@
 require("__LarkenxK2SETweaks__/data/K2SE_logistic_pipes_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/SE_buildings_in-space_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/K2SE_buildings_lab.data-final-fixes")
+require("__LarkenxK2SETweaks__/data/SE_buildings_on-spaceship_data-final-fixes")
 
 if mods["space-exploration"] and mods["Krastorio2"] then
     if settings.startup["allow-remove-tesseract-from-stabilizer"].value then
@@ -10,31 +11,6 @@ if mods["space-exploration"] and mods["Krastorio2"] then
     if settings.startup["allow-remove-stabilizer-for-vulcanite"].value then
         util.remove_ingredient(data.raw["recipe"]["matter-to-se-vulcanite"], "charged-matter-stabilizer")
         util.remove_result(data.raw["recipe"]["matter-to-se-vulcanite"], "matter-stabilizer")
-    end
-
-    if settings.startup["allow-hypercooler-on-spaceship"].value then
-        local allowed_collision_mask_values = {
-            "water-tile",
-            "ground-tile",
-            "item-layer",
-            "object-layer",
-            "player-layer"
-        }
-        -- remove the spaceship collision layer from the hypercooler
-        data.raw["assembling-machine"]["se-space-hypercooler"].collision_mask = allowed_collision_mask_values
-        data.raw["assembling-machine"]["se-space-hypercooler"].se_allow_in_space = true
-        -- in addition, the particle accelerator & thermal radiator recipes get disabled when grounded. causes some issues
-        -- when landing because it can delete ingredients currently active
-        local defaultSpaceRadiator = data.raw["assembling-machine"]["se-space-radiator"]
-        local groundedSpaceRadiator = data.raw["assembling-machine"]["se-space-radiator-grounded"]
-        groundedSpaceRadiator.crafting_categories = defaultSpaceRadiator.crafting_categories
-        local defaultSpaceRadiator2 = data.raw["assembling-machine"]["se-space-radiator-2"]
-        local groundedSpaceRadiator2 = data.raw["assembling-machine"]["se-space-radiator-2-grounded"]
-        groundedSpaceRadiator2.crafting_categories = defaultSpaceRadiator2.crafting_categories
-
-        local defaultAccelerator = data.raw["assembling-machine"]["se-space-particle-accelerator"]
-        local groundedAccelerator = data.raw["assembling-machine"]["se-space-particle-accelerator-grounded"]
-        groundedAccelerator.crafting_categories = defaultAccelerator.crafting_categories
     end
 
     -- ===================================================================================================
