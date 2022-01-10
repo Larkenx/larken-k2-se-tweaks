4463a4a0e972abb456d54ffc7bb8c79ba82bfd08
Nicolas Lang <git@nicolas-lang.de>
2022-01-10 03:04:50 +0100

    moved matter-cube changes to K2SE_matterconversion_cubes.data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,6 +2,7 @@ require("__LarkenxK2SETweaks__/data/K2SE_logistic_pipes_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/SE_buildings_in-space_data-final-fixes")
 require("__LarkenxK2SETweaks__/data/K2SE_buildings_lab.data-final-fixes")
 require("__LarkenxK2SETweaks__/data/SE_buildings_on-spaceship_data-final-fixes")
+require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_cubes.data-final-fixes")
 
 if mods["space-exploration"] and mods["Krastorio2"] then
     if settings.startup["allow-remove-tesseract-from-stabilizer"].value then
@@ -43,29 +44,6 @@ if mods["space-exploration"] and mods["Krastorio2"] then
                         ingredient.amount = math.floor(0.5 + ingredient.amount * nerf_factor)
                         if ingredient.catalyst_amount then
                             ingredient.catalyst_amount = math.floor(0.5 + ingredient.catalyst_amount * nerf_factor)
-                        end
-                    end
-                end
-            end
-        end
-    end
-
-    local total_matter_capacity = 1000 -- default k2 ingredient count
-    local matter_cube_cost = 1000 -- default 1:1 ratio for conversion and deconversion
-    if settings.startup["matter-cube-cost"] then
-        matter_cube_cost = settings.startup["matter-cube-cost"].value
-    end
-    if settings.startup["matter-cube-density"] then
-        total_matter_capacity = settings.startup["matter-cube-density"].value
-    end
-    for _, recipe in pairs(data.raw.recipe) do
-        if recipe.category == "matter-deconversion" then
-            if recipe.ingredients ~= nil then
-                for _, ingredient in pairs(recipe.ingredients) do
-                    if ingredient.name == "matter" then
-                        if recipe.main_product == "matter-cube" then
-                            ingredient.amount = total_matter_capacity
-                            ingredient.catalyst_amount = matter_cube_cost
                         end
                     end
                 end
