5494e8aa5a017c81a32d7d8f8463dae4d7a8fb08
Nicolas Lang <git@nicolas-lang.de>
2022-01-10 01:44:36 +0100

    moved vitamelange-processing recipe and tech to K2SE_matterconversion_vitamelange_data.lua

--- data.lua
+++ data.lua
@@ -1,3 +1,5 @@
+require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_vitamelange_data")
+
 local matter_util = require("__LarkenxK2SETweaks__/data/K2SE_matterconversion_util")
 require("__Krastorio2__/lib/public/data-stages/matter-util")
 -- this multiplayer comes straight from space exploration's matter.lua. same as holm
@@ -8,20 +10,6 @@ if mods["Krastorio2"] then
 end
 
 if mods["space-exploration"] and mods["Krastorio2"] then
-    local matterIcon
-    matter_util.make_matter_tech("se-kr-matter-vitamelange-processing", "vitamelange-processing", 500)
-    krastorio.matter_func.createMatterRecipe(
-        {
-            item_name = "se-vitamelange",
-            minimum_conversion_quantity = 10,
-            matter_value = value_multiplier * 7 * 8, -- x8
-            conversion_matter_value = value_multiplier * 7,
-            energy_required = value_multiplier * 7,
-            need_stabilizer = true,
-            unlocked_by_technology = "se-kr-matter-vitamelange-processing"
-        }
-    )
-
     if settings.startup["allow-naquium-matter-processing"].value then
         matter_util.make_matter_tech("se-kr-matter-naquium-processing", "naquium-processing", 1000)
         krastorio.matter_func.createMatterRecipe(
